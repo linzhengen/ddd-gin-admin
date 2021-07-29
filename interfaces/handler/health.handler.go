3fed3a26dc3b7745101d6e9da2d4421b4cc39578
linzhengen <linzhengen@yahoo.co.jp>
2021-07-29 18:24:15 +0900

    fix lint

--- interfaces/handler/health.handler.go
+++ interfaces/handler/health.handler.go
@@ -1,11 +1,12 @@
 package handler
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/wire"
 	"github.com/linzhengen/ddd-gin-admin/domain/schema"
 	"github.com/linzhengen/ddd-gin-admin/infrastructure/ginx"
-	"time"
 )
 
 var HealthCheckSet = wire.NewSet(wire.Struct(new(HealthCheck), "*"))
@@ -15,7 +16,7 @@ type HealthCheck struct {
 
 func (a *HealthCheck) Get(c *gin.Context) {
 	ginx.ResSuccess(c, &schema.HealthCheck{
-		Status: "OK",
+		Status:    "OK",
 		CheckedAt: time.Now(),
 	})
 }
